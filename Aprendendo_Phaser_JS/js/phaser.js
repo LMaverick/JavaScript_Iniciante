236cfb4ce2bbb84e7cc382e3c4dc0235c88b533d
LMaverick <91562063+LMaverick@users.noreply.github.com>
2022-01-07 14:52:03 -0300

    Update phaser.js

--- Aprendendo_Phaser_JS/js/phaser.js
+++ Aprendendo_Phaser_JS/js/phaser.js
@@ -1,5 +1,28 @@
 (function(){
  var game = new Phaser.Game(800,600,Phaser.AUTO,null,{
-     
+     preload: preload,
+     create:create,
+     update:update 
  });
+
+ //===================================
+ //=============preload===============
+ function preload(){
+    game.load.image('sky','img/sky.png');
+ }
+
+ //===================================
+ //=============create===============
+ function create(){
+     game.add.sprite(0,0,'sky');
+
+ }
+
+ //===================================
+ //=============update===============
+ function update(){
+
+ }
+
+
 }());
